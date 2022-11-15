78938eb44fac0f0620075bb23c1e532259eef45d
MilimRajaIblis <118158607+MilimRajaIblis@users.noreply.github.com>
2022-11-15 11:55:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,30 +26,30 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "DimasBotz" //namabot kalian
-global.ownername= "𝐃𝐈𝐌𝐀𝐒 𝐓𝐎𝐊𝐈𝐒𝐀𝐊𝐈" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
+global.botname = "IciwikirBot" //namabot kalian
+global.ownername= "Ferlian X Xzy" //nama kalian
+global.myweb ="https://api.xzyyyofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g" //bebas asal jan hapus
 global.myweb2 = "https://instagram.com/leviatnansyah?igshid=YmMyMTA2M2Y=" //bebas
 global.email = "leviadnansyahlevi@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['6283169648444'] //premium user
-global.owner = ["6283169648444","6283169648444"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6283169648444" // nomor wa kalian
-global.ownernomerr = "6283169648444" //nmr wa kalian
+global.owner = ["6289653436302","6283190750185"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289653436302" // nomor wa kalian
+global.ownernomerr = "6283190750185" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© DimasBotz' //sticker wm ubah
-global.author = 'Di Buat Oleh 𝐃𝐈𝐌𝐀𝐒 𝐓𝐎𝐊𝐈𝐒𝐀𝐊𝐈' //sticker wm ganti nama kalian
+global.packname = '© IcikiwirBotz' //sticker wm ubah
+global.author = 'Di Buat Oleh Ferlian' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
     premium: "10000",
-    free: 100
+    free: 10000
 }
 
 global.adventureRPG = 'https://telegra.ph/file/ff94536d69e0f4f3e7b54.jpg'
